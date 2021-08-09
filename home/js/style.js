6812b819a76bac8a3b2d50e5e28e45ba1a83cd76
= <web.dev.trabalho@gmail.com>
2021-08-09 01:20:37 -0300

    primeiro commit
    primeiro commit

---
+++ home/js/style.js
@@ -0,0 +1,13 @@
+let nav = document.querySelector('nav');
+let windowWidth = window.innerWidth;
+let windowHeight = window.innerHeight;
+
+/**
+
+if (windowWidth > 768) {
+    nav.style.backgroundColor = 'black';
+} else {
+    nav.style.backgroundColor = 'yellow';
+}
+
+*/
